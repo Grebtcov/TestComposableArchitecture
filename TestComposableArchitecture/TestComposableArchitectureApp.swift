9c65fd25f51cc022d66e3538b9a6d09584256348
Arts <ryllezz55@gmail.com>
2022-12-03 18:16:15 +0500

    Initial Commit

---
+++ TestComposableArchitecture/TestComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestComposableArchitectureApp.swift
+//  TestComposableArchitecture
+//
+//  Created by Alex x on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TestComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
